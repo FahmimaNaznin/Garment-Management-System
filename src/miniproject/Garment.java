e56014b3eb9f71534aa58dd2f7151181a300cbc9
FahmimaNaznin <naznin23105101248@diu.edu.bd>
2024-11-15 22:10:06 +0600

    Update

---
+++ src/miniproject/Garment.java
@@ -0,0 +1,43 @@
+
+package miniproject;
+
+
+public class Garment {
+    class Garment {
+
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+
+    void updateStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
+
+    double calculateDiscountPrice(double discountPercentage) {
+        double discount = price * (discountPercentage / 100);
+        return discount;
+    }
+}
+
+
+
+    
+public class OopLabTask3 {
+
+    public static void main(String[] args) {
+        Garment g1 = new Garment();
+        g1.name = "Silk";
+        g1.description = "Good Product";
+        g1.price= 600;
+        double x = g1.calculateDiscountPrice(10);
+        System.out.println(x);
+
+    }
+
+}
+    
+}
